fcf09c0ea15595b4d8e3114c508e203dc6c6599e
jthalacker <jthalacker@EPM-HSDYF12.ad.wisc.edu>
2019-04-29 16:02:12 -0500

    QuestionScene commit 2

--- application/QuestionScene.java
+++ application/QuestionScene.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -16,6 +17,7 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -33,27 +35,24 @@ public class QuestionScene extends Application {
 	//Fields
 
 	Question question; //This will be the current question it is displaying
-	QuestionBank currTopics;
+	QuestionBank currQuestions;
+	List<Answer> answers;  //List of answers
+	List<RadioButton> answerButtons; //list of answer radio buttons
+	int numQuizQuestions;  //Total number of questions
+	int questionNumber;  //current question displayed
 
 
-	QuestionScene(QuestionBank currTopics){
-		question = currTopics.getNextQuestion();
-		this.currTopics = currTopics;
+	QuestionScene(QuestionBank currQuestions, int questionNumber){
+		question = currQuestions.questions.get(questionNumber);
+		this.currQuestions= currQuestions;
+		answers = question.getAnswersList();
+		this.questionNumber = questionNumber;
+		this.numQuizQuestions = currQuestions.questions.size();
 	}
 
-	//Any Object that has an action event is intialized as a field
-	Button addAnswerButton = new Button("Add Answer");
-	Button imagePickButton = new Button("Find Image"); //Opens a file reader
-	Button backButton = new Button("Back"); //Goes to main page
-	Button makeQuestion = new Button("Make Question"); //Make question then go back to main page
-
-	//List of answers for the question
-	ArrayList<Answer> answerListObject = new ArrayList<Answer>();	
 
 	//File for image needed publically
 	File image;
-
-	//Constructor // None needed?
 
 
 	//Methods
@@ -67,13 +66,28 @@ public class QuestionScene extends Application {
 			//Create both sides
 			VBox vBox1 = new VBox(15);
 			VBox vBox2 = new VBox(15);
+			
 
+			//Create Objects to put in the boxes
 
+			//Create Label for Question Number
+			Label questionNumberLabel = new Label("Question: " + questionNumber + 1 + "/" + numQuizQuestions);
+			
+			//Create question text label
+			Label questionText = new Label(question.getQuestionText());
+			questionText.setWrapText(true);
+			
+			//Create the radio buttons with the answer values for the questions
+			for(int i = 0; i < answers.size(); i++) {
+				answerButtons.add(new RadioButton(answers.get(i).getAnswerText()));
+			}
 
+			
+			
 			//Create scene and update stage
 			Scene scene = new Scene(root,550, 700);
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Make Question");
+			primaryStage.setTitle("Question Scene");
 			primaryStage.show();
 
 		} catch(Exception e) {
@@ -87,13 +101,15 @@ public class QuestionScene extends Application {
 	 */
 	private Boolean checkAnswer(Answer answer) {
 
+		return false;
 	}
 	/**
 	 * This method returns the scene back to the main screen
-	 * It will be called on quit, or when quiz is over
+	 * It will be called on quit
 	 */
 	private void backToMain() {
-
+		
+		return;
 	}
 	/**
 	 * This method will update the QuizResult class with whether the question
